593a7216202ed444212c0c43028a58aae3888e3f
karthikm999 <keerthik.km95@gmail.com>
2018-07-06 19:41:08 -0700

    my vim commands

---
+++ vim_commands.py
@@ -0,0 +1,12 @@
+h - move cursor left
+j - move cursor down
+k - move cursor up
+l - move cursor right
+H - move to top of screen
+M - move to middle of screen
+L - move to bottom of screen
+w - jump forwards to the start of a word
+W - jump forwards to the start of a word (words can contain punctuation)
+e - jump forwards to the end of a word
+E - jump forwards to the end of a word (words can contain punctuation)
+b - jump backwards to the start of a word
