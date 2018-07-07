2762a18affc7ab59f86ca0b249e7aea817fe89a5
karthikm999 <keerthik.km95@gmail.com>
2018-07-06 19:40:21 -0700

    my bash commands

---
+++ bash_commands.py
@@ -0,0 +1,10 @@
+ls - list of files in a directory
+mkdir - used to make new directory
+cd - used to change the directory
+rmdir - used to remove directory
+cp - used to copies a file/directory
+mv - used to move/rename a file/directory
+rm - used to delete a file
+touch - used to update the last-modified time of a file
+cat - used to output the contents of a file
+pwd - used to displays the shell's current working directory
