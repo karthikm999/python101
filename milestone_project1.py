ef649d62a8012a5981ffea682faa51215c71aeed
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-16 20:32:31 -0700

    Update milestone_project1.py

--- milestone_project1.py
+++ milestone_project1.py
@@ -43,6 +43,13 @@ else:
 sentence=input("enter a words:")
 word=sentence.split()
 word.sort()
-print("the sorted words are:")
 for i in word:
     print(i)
+    
+# Factorial
+
+n =int(input("enter a number:"))
+fact = 1
+for i in range(1,n+1):
+    fact = fact * i
+print(fact)
