db23fb6a236a1b608e22042dc475b15f898cb20a
karthikm999 <keerthik.km95@gmail.com>
2018-07-19 20:49:23 -0700

    my ex22 file

---
+++ ex22.py
@@ -0,0 +1,21 @@
+def lcm(x, y):
+   """This function takes two
+   integers and returns the L.C.M."""
+   if x > y:
+       greater = x
+   else:
+       greater = y
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+   return lcm
+num1 = 67
+num2 = 34
+
+# uncomment the following lines to take input from the user
+#num1 = int(input("Enter first number: "))
+#num2 = int(input("Enter second number: "))
+
+print("The L.C.M. of", num1,"and", num2,"is", lcm(num1, num2))
