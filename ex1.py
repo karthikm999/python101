c40777fb0655c09459b5dc4e270050c7b1cd9197
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 18:54:27 +0530

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,8 +1,15 @@
 #good first program
 print("Hello World!")
+#output: Hello World!
 print("Hello Again")
+#output: Hello Again
 print("I like typing this.")
+#output: I like typing this.
 print("This is fun.")
+#output: This is fun.
 print('Yay! Printing.')
+#output: Yay! Printing.
 print("I'd much rather you 'not'.")
+#output: I'd much rather you 'not'.
 print('I "said" do not touch this.')
+#output: I "said" do not touch this.
