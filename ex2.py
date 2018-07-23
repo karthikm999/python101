0681070f6e77f855fc781a9416ecfbea542de1d0
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 18:57:04 +0530

    ex2.py

--- ex2.py
+++ ex2.py
@@ -8,3 +8,7 @@ print("I could have code like this.") # and the comment after is ignored
 # print("This won't run.")
 
 print("This will run.")
+
+#output
+I could have code like this.
+This will run.
