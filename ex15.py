053975032ba033f20ba15116c40a49e606a7c744
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 21:27:52 +0530

    my ex15 file

--- ex15.py
+++ ex15.py
@@ -1,23 +1,40 @@
-people = 30
-cars = 40
-trucks = 15
-
-
+people = 25
+cars = 53
+trucks = 14
 if cars > people:
     print("We should take the cars.")
 elif cars < people:
     print("We should not take the cars.")
 else:
     print("We can't decide.")
-
 if trucks > cars:
     print("That's too many trucks.")
 elif trucks < cars:
     print("Maybe we could take the trucks.")
 else:
     print("We still can't decide.")
-
 if people > trucks:
     print("Alright, let's just take the trucks.")
 else:
     print("Fine, let's stay home then.")
+    
+#output
+We should take the cars.
+Maybe we could take the trucks.
+Alright, let's just take the trucks.
+
+#if people = 20
+cars = 20
+trucks = 14
+#output:
+We can't decide.
+Maybe we could take the trucks.
+Alright, let's just take the trucks.
+
+# if people = 20
+cars = 20
+trucks = 35
+#output:
+We can't decide.
+That's too many trucks.
+Fine, let's stay home then.
