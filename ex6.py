5736324680d957a717a85792d39e9eb88f95b3fb
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 19:22:55 +0530

    ex6.py

--- ex6.py
+++ ex6.py
@@ -2,3 +2,7 @@
 default_order = "{}, {} and {}".format('Chicken','Mutton','Fish')
 print('\n--- Default Order ---')
 print(default_order)
+
+#output:
+--- Default Order ---
+Chicken, Mutton and Fish
