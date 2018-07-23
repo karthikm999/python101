40e46ae625267c007cbc70198acfb20e6937f2bb
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 19:25:02 +0530

    ex6_1.py

--- ex6_1.py
+++ ex6_1.py
@@ -1,4 +1,9 @@
 # order using positional argument
 positional_order = "{1}, {0} and {2}".format('Chicken','Mutton','Fish')
-print('\n--- Positional Order ---')
+print('\n----- Positional Order -----')
 print(positional_order)
+
+#output:
+
+----- Positional Order -----
+Mutton, Chicken and Fish
