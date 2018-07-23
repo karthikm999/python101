65d1d70a3bbf231e58a02eb0bbcca9daf20535c3
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 19:35:53 +0530

    ex8.py

--- ex8.py
+++ ex8.py
@@ -1,22 +1,24 @@
 types_of_people = 10
 x = f"There are {types_of_people} types of people."
-
 binary = "binary"
 do_not = "don't"
 y = f"Those who know {binary} and those who {do_not}."
-
 print(x)
 print(y)
-
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
-
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
-
 print(joke_evaluation.format(hilarious))
-
 w = "This is the left side of..."
 e = "a string with a right side."
-
 print(w + e)
+
+#output:
+
+There are 10 types of people.
+Those who know binary and those who don't.
+I said: There are 10 types of people.
+I also said: 'Those who know binary and those who don't.'
+Isn't that joke so funny?! False
+This is the left side of...a string with a right side.
