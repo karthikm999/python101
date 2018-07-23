34e60f15634f3af75ca3077ed5e7b6f24d26f57e
karthikm999 <40877604+karthikm999@users.noreply.github.com>
2018-07-23 19:00:46 +0530

    ex3.py

--- ex3.py
+++ ex3.py
@@ -22,3 +22,18 @@ print("Is it greater", 5 > -2)
 print("Is it greater or equal", 5 >= -2)
 print("Is it less or equal", 5 <= -2)
 
+#output:
+I will now count my chickens:
+Hens 30.0
+Roosters 97
+Now I will count the eggs:
+6.75
+Is it true that 3 + 2 < 5 - 7
+False
+What is 3 + 2? 5
+What is 5 - 7? -2
+Oh, that's why it's False.
+How about some more.
+Is it greater True
+Is it greater or equal True
+Is it less or equal False
